e52c2e971b2706b43d583d88f3f43deb0c6b1bb0
ErnWong <ErnWong@users.noreply.github.com>
2021-10-05 09:51:43 +0000

    deploy: 145041c92c9a48d3b2ee3c4e31075eded6441d12

--- index-279af9028938999e.js
+++ index-279af9028938999e.js
@@ -274,7 +274,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-a4daaf821b2b7d38_bg.wasm', import.meta.url);
+        input = new URL('index-279af9028938999e_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
